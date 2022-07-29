6441b1f98aea359e05bb95c257449ebcfa4556f3
Kilian Gosewisch <koriwi@users.noreply.github.com>
2022-07-29 21:49:16 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,27 +2,25 @@ export const DEV_MODE = process.env.NODE_ENV !== 'production';
 
 export const GUILD_ID = DEV_MODE
 	? process.env.DEV_GUILD_ID
-	: '616186924390023171';
+	: '';
 
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
-export const TAURI_BLUE = 0x67d6ed;
+export const TAURI_BLUE = 0x754BAB;
 
 // people
 const ADMIN_ROLES = DEV_MODE
 	? [process.env.DEV_ADMIN_ROLE]
 	: [
 			// admin
-			'985400380663935088',
+			'742491247481978901',
 			// core
-			'616187491715907585',
-			// working-group
-			'761977421305610241',
+			'738873387908333610'
 	  ];
 
 export const BOT_DEVS = [
-	// LorenzoLewis
-	'402698003569180674',
+	// Thijs
+	'361818385811505153',
 ];
 
 //  list of roles/user IDs other than the creator allowed to modify threads
@@ -32,8 +30,8 @@ export const THREAD_ADMIN_IDS = [...ADMIN_ROLES, ...BOT_DEVS];
 export const AUTO_THREAD_CHANNELS = DEV_MODE
 	? [process.env.DEV_HELP_CHANNEL]
 	: [
-			// #help-triage
-			'625037620996734986',
+			// #help
+			'699366419485163660',
 	  ];
 
 export const REACTION_ROLE: {
